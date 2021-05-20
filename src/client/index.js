b1f0027a9c19d1bbdd39bd2530036cdd2dd90b81
willian Hilton <willian.hilton@hotmail.com>
2021-05-20 20:14:21 -0300

    Inclusão do header

--- src/client/index.js
+++ src/client/index.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 const client = axios.create({
   baseURL: "https://pokersnts-prd.us-east-2.elasticbeanstalk.com",
   headers: {
-    "Access-Control-Allow-Origin": "*"
+    "Access-Control-Allow-Origin": "*",
+    "Header set Access-Control-Allow-Methods": "POST, GET, PUT, DELETE, OPTIONS",
+    "Header add Access-Control-Allow-Headers": "Authorization, Content-Type, Accept",
+    "Header set Access-Control-Allow-Credentials": true
   }
 });
 
