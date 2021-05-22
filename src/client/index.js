644e20c682b82844fa7e872214ce4c1d6b259213
willian Hilton <willian.hilton@hotmail.com>
2021-05-22 00:09:49 -0300

    fix: Remoção da tag meta e alteração da api para https

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: "http://pokersnts-prd.us-east-2.elasticbeanstalk.com"
+  baseURL: "https://pokersnts-prd.us-east-2.elasticbeanstalk.com"
 });
 
 export default client;
